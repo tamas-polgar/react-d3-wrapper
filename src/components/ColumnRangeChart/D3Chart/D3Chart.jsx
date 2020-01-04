4d06d9dd9deaa33e44eed7612150d4117da1a98b
tamas-polgar <tamas.polgar.dev@gmail.com>
2020-01-04 19:54:36 +0000

    Data connection for ColumnRangeChart created

--- src/components/ColumnRangeChart/D3Chart/D3Chart.jsx
+++ src/components/ColumnRangeChart/D3Chart/D3Chart.jsx
@@ -1,13 +1,12 @@
 import * as d3 from 'd3';
 
-const margin = { top: 10, bottom: 80, left: 70, right: 10 }
-const width = 500 - margin.left - margin.right;
-const height = 300 - margin.top - margin.bottom;
+const margin = { top: 10, bottom: 80, left: 70, right: 10 };
+const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
 
-class D3Chart {
+export default class D3Chart {
 	constructor(element) {
 		let vis = this
-
 		vis.g = d3.select(element)
 			.append("svg")
 				.attr("width", width + margin.left + margin.right)
@@ -15,7 +14,33 @@ class D3Chart {
 			.append("g")
 				.attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-		vis.update()		
+		vis.xLabel = vis.svg.append('text')
+            .attr('x', width /2)
+            .attr('y', height + 50)
+            .attr('text-anchor', 'middle')
+
+        vis.svg.append('text')
+            .attr('x', -(height / 2))
+            .attr('y', -50)
+            .attr('text-anchor', 'middle')
+            .text('Height in cm')
+            .attr('transform', 'rotate(-90)')
+
+        vis.xAxisGroup = vis.svg.append('g')
+            .attr('transform', `translate(0, ${height})`)
+
+        vis.yAxisGroup =  vis.svg.append('g')
+
+		Promise.all([
+			d3.json('https://d3-datasets.firebaseio.com/first_year_data.json'),
+			d3.json('https://d3-datasets.firebaseio.com/second_year_data.json'),
+			d3.json('https://d3-datasets.firebaseio.com/third_year_data.json')
+		]).then((dataSets) => {
+			vis.firstYearData = dataSets[0]
+			vis.secondYearData = dataSets[1]
+			vis.thirdYearData = dataSets[2]
+			vis.update()
+		})		
 	}
 
 	update() {
@@ -23,5 +48,3 @@ class D3Chart {
 	
 	}
 }
-
-export default D3Chart;
